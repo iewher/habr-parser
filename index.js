e72ea485d3a972470ae56d5017f5df466988d454
iewher <goshagang@mail.ru>
2023-10-01 20:37:33 +0500

    added entry to text file

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-console.log("parser started");
-
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
-(async () => {
+const parser = async () => {
+  console.log("parser started");
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://github.com/iewher");
-  await page.screenshot({ path: "img.png" });
+  console.log("20%");
+  await page.goto(
+    "https://career.habr.com/vacancies?page=1&q=react&type=suitable"
+  );
+  // await page.screenshot({ path: "img.png" });
+  console.log("40%");
 
   const arr = await page.evaluate(() => {
-    let text = Array.from(
-      document.querySelectorAll(".repo"),
+    const text = Array.from(
+      document.querySelectorAll(".vacancy-card__title-link"),
       (el) => el.innerText
     );
+
+    console.log("60%");
 
     return text;
   });
 
-  console.log(arr);
+  fs.writeFileSync("company.txt", arr.join("\n"), "utf-8");
+  console.log("80%");
+
   await browser.close();
-})();
+  console.log("parser finished");
+};
+
+parser();
