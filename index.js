92da4fc298c5190ff124349d578a71b6dc5b4c8b
iewher <goshagang@mail.ru>
2023-10-02 03:38:44 +0500

    added open each vacancies

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const cliProgress = require("cli-progress");
+const { log } = require("console");
 
 const parser = async () => {
-  console.log("parser started");
+  const bar1 = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic
+  );
+  console.log("Парсер запущен");
+  bar1.start(100, 0);
   const browser = await puppeteer.launch({ headless: false });
+  bar1.update(20);
   const page = await browser.newPage();
-  console.log("20%");
   await page.goto(
     "https://career.habr.com/vacancies?page=1&q=react&type=suitable"
   );
-  // await page.screenshot({ path: "img.png" });
-  console.log("40%");
+  bar1.update(40);
 
-  const arr = await page.evaluate(() => {
-    const text = Array.from(
+  const array_name = await page.evaluate(() => {
+    const name = Array.from(
       document.querySelectorAll(".vacancy-card__title-link"),
       (el) => el.innerText
     );
 
-    console.log("60%");
-
-    return text;
+    return name;
   });
 
-  fs.writeFileSync("company.txt", arr.join("\n"), "utf-8");
-  console.log("80%");
+  const array_links = await page.evaluate(() => {
+    const links = Array.from(
+      document.querySelectorAll(".vacancy-card__title-link"),
+      (el) => el.getAttribute("href")
+    );
+
+    return links;
+  });
+
+  bar1.update(60);
+
+  const array_combined = array_name.map(
+    (name, index) => `${name} - https://career.habr.com${array_links[index]}`
+  );
+  fs.writeFileSync("vacancies.txt", array_combined.join("\n"), "utf-8");
+
+  const new_pages = [];
+  for (let i = 0; i < array_links.length; i++) {
+    const new_page = await browser.newPage();
+    await new_page.goto(`https://career.habr.com${array_links[i]}`);
+    // const array_mail = await page.evaluate(() => {
+    //   const mail = Array.from(document.querySelectorAll(".link-comp"), (el) =>
+    //     el.getAttribute("href")
+    //   );
+
+    //   return mail;
+    // });
+
+    // console.log(array_mail);
+    new_pages.push(new_page);
+  }
+  await Promise.all(new_pages);
+
+  bar1.update(100);
+  bar1.stop();
 
   await browser.close();
-  console.log("parser finished");
+  console.log("Создан текстовый файл");
+  console.log("Парсер закончил работу успешно");
 };
 
 parser();
+
+//TODO: Переходим по каждой ссылке при парсинге страницы, забираем оттуда сайт (на habr career есть у каждой компании), переходим на страницу, забираем DOM обьект с словом mailto
