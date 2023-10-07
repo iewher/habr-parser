77529d08d6a962238df39743608939fbff624f65
iewher <goshagang@mail.ru>
2023-10-07 21:40:37 +0500

    added cycle for go to all page

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const parser = async () => {
     sites: [],
     mail: [],
   };
+  let current_page = 1;
 
   const bar1 = new cliProgress.SingleBar(
     {
@@ -41,106 +42,110 @@ const parser = async () => {
   const browser = await puppeteer.launch({ headless: false });
   bar1.update(20);
 
-  const page = await browser.newPage();
-  await page.goto(
-    "https://career.habr.com/vacancies?page=1&q=react&type=suitable"
-  );
-  bar1.update(40);
-
-  const array_name = await page.evaluate(() => {
-    const name = Array.from(
-      document.querySelectorAll(".vacancy-card__title-link"),
-      (el) => el.innerText
+  for (let circle = 0; circle < 5; circle++) {
+    const page = await browser.newPage();
+    await page.goto(
+      `https://career.habr.com/vacancies?page=${current_page}&q=react&type=suitable`
     );
-    return name;
-  });
+    bar1.update(40);
 
-  const array_links = await page.evaluate(() => {
-    const links = Array.from(
-      document.querySelectorAll(".vacancy-card__title-link"),
-      (el) => el.getAttribute("href")
-    );
-    return links;
-  });
-
-  bar1.update(60);
-
-  const links_combined = array_name.map(
-    (name, index) =>
-      `${index + 1}. ${name} - https://career.habr.com${array_links[index]}`
-  );
-
-  data.vacancies = links_combined.map((link) => link.replace(/ - /, " - "));
-  fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
-
-  await page.close();
-
-  for (let i = 0; i < array_links.length; i++) {
-    const new_page = await browser.newPage();
-    await new_page.goto(`https://career.habr.com${array_links[i]}`);
-
-    const array_site = await new_page.evaluate(() => {
-      const site_company = document.querySelector(".company_site");
-      return site_company ? site_company.innerText : null;
+    const array_name = await page.evaluate(() => {
+      const name = Array.from(
+        document.querySelectorAll(".vacancy-card__title-link"),
+        (el) => el.innerText
+      );
+      return name;
     });
 
-    if (array_site && !all_sites.includes(array_site)) {
-      all_sites.push(array_site);
-    }
-
-    new_pages.push(new_page);
-
-    bar1.update(60 + ((i + 1) / array_links.length) * 20);
-
-    await new_page.close();
-  }
-
-  data.sites = all_sites;
-  fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
-
-  const site_urls = all_sites.map(
-    (site, index) => `${index + 1}. https://${site}`
-  );
-
-  await Promise.all(new_pages);
-
-  for (let i = 0; i < all_sites.length; i++) {
-    try {
-      const new_page = await browser.newPage();
-      await new_page.goto(`https://${all_sites[i]}`);
-
-      fs.appendFileSync(
-        "log.txt",
-        `${date} - Страница успешно открыта ${all_sites[i]}\n`,
-        "utf-8"
+    const array_links = await page.evaluate(() => {
+      const links = Array.from(
+        document.querySelectorAll(".vacancy-card__title-link"),
+        (el) => el.getAttribute("href")
       );
+      return links;
+    });
 
-      const hrefValue = await new_page.evaluate(() => {
-        const elementsWithHref = document.querySelectorAll('[href*="mailto"]');
-        if (elementsWithHref.length > 0) {
-          return elementsWithHref[0].getAttribute("href");
-        }
-        return null;
+    bar1.update(60);
+
+    const links_combined = array_name.map(
+      (name, index) =>
+        `${index + 1}. ${name} - https://career.habr.com${array_links[index]}`
+    );
+
+    data.vacancies = links_combined.map((link) => link.replace(/ - /, " - "));
+    fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
+
+    await page.close();
+
+    for (let i = 0; i < array_links.length; i++) {
+      const new_page = await browser.newPage();
+      await new_page.goto(`https://career.habr.com${array_links[i]}`);
+
+      const array_site = await new_page.evaluate(() => {
+        const site_company = document.querySelector(".company_site");
+        return site_company ? site_company.innerText : null;
       });
 
-      if (hrefValue !== null) {
-        all_mail.push(hrefValue.replace(/mailto:/g, ""));
+      if (array_site && !all_sites.includes(array_site)) {
+        all_sites.push(array_site);
       }
 
+      new_pages.push(new_page);
+
+      bar1.update(60 + ((i + 1) / array_links.length) * 20);
+
       await new_page.close();
-    } catch (error) {
-      fs.appendFileSync(
-        "error.txt",
-        `${date} - Ошибка при открытии страницы ${all_sites[i]}. ${error.message}\n`,
-        "utf-8"
-      );
     }
 
-    bar1.update(80 + ((i + 1) / all_sites.length) * 20);
-  }
+    data.sites = all_sites;
+    fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
 
-  data.mail = all_mail;
-  fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
+    const site_urls = all_sites.map(
+      (site, index) => `${index + 1}. https://${site}`
+    );
+
+    await Promise.all(new_pages);
+
+    for (let i = 0; i < all_sites.length; i++) {
+      try {
+        const new_page = await browser.newPage();
+        await new_page.goto(`https://${all_sites[i]}`);
+
+        fs.appendFileSync(
+          "log.txt",
+          `${date} - Страница успешно открыта ${all_sites[i]}\n`,
+          "utf-8"
+        );
+
+        const hrefValue = await new_page.evaluate(() => {
+          const elementsWithHref =
+            document.querySelectorAll('[href*="mailto"]');
+          if (elementsWithHref.length > 0) {
+            return elementsWithHref[0].getAttribute("href");
+          }
+          return null;
+        });
+
+        if (hrefValue !== null) {
+          all_mail.push(hrefValue.replace(/mailto:/g, ""));
+        }
+
+        await new_page.close();
+      } catch (error) {
+        fs.appendFileSync(
+          "error.txt",
+          `${date} - Ошибка при открытии страницы ${all_sites[i]}. ${error.message}\n`,
+          "utf-8"
+        );
+      }
+
+      bar1.update(80 + ((i + 1) / all_sites.length) * 20);
+    }
+
+    data.mail = all_mail;
+    fs.writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
+    current_page++;
+  }
 
   bar1.update(100);
   bar1.stop();
