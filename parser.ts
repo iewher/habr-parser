371b4fdab209399e16325e4ceb23ed2f8617db94
iewher <goshagang@mail.ru>
2024-05-13 17:51:44 +0500

    feat: parser move from pupeeter to cheerio

--- parser.ts
+++ parser.ts
@@ -1,10 +1,9 @@
-const puppeteer = require("puppeteer");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const fs = require("fs");
 const cliProgress = require("cli-progress");
 
 const parser = async () => {
-  const new_pages = [];
-  const pagePromises = [];
   const all_sites = [];
   const all_mail = [];
   const data = {
@@ -15,13 +14,8 @@ const parser = async () => {
   let links_combined = [];
   let all_links = [];
 
-  const bar1 = new cliProgress.SingleBar(
-    {
-      format:
-        "[{bar}] {percentage}% | Завершено: {value}/{total} | Осталось времени: {eta_formatted}",
-    },
-    cliProgress.Presets.shades_classic,
-  );
+  // Изменить, если нужно парнить не 100 страниц
+  const value = 2;
 
   const time = new Date();
   const date = `[${time.getHours()}h : ${time.getMinutes()}m : ${time.getSeconds()}s]`;
@@ -39,196 +33,104 @@ const parser = async () => {
   console.log(logo);
   console.log("Парсер запущен\n");
 
-  bar1.start(100, 0);
+  console.log("\n\n1. Получаем список вакансий\n\n");
 
-  const browser = await puppeteer.launch({ headless: false });
-  bar1.update(20);
+  for (let i = 1; i <= value; i++) {
+    const response = await axios.get(
+      `https://career.habr.com/vacancies?page=${i}&type=all`,
+    );
 
-  const processPagesConcurrently = async (
-    startPage,
-    endPage,
-    concurrentPages,
-  ) => {
-    for (
-      let current_page = startPage;
-      current_page <= endPage;
-      current_page += concurrentPages
-    ) {
-      const pagePromises = [];
+    console.log(`\n\nСтраница ${i}/${value}\n\n`);
 
-      for (let i = 0; i < concurrentPages; i++) {
-        const page = current_page + i;
-        if (page <= endPage) {
-          pagePromises.push(processPage(page));
-        }
-      }
+    const chr = cheerio.load(response.data);
 
-      await Promise.all(pagePromises);
-    }
-  };
-
-  const processSitesConcurrently = async (all_links, concurrentPages) => {
-    for (let i = 0; i < all_links.length; i += concurrentPages) {
-      const pagePromises = [];
-
-      for (let j = 0; j < concurrentPages; j++) {
-        const linkIndex = i + j;
-        if (linkIndex < all_links.length) {
-          pagePromises.push(processSite(all_links[linkIndex]));
-        }
-      }
-
-      for (const pagePromise of pagePromises) {
-        await pagePromise;
-      }
-    }
-  };
-
-  const processEmailsConcurrently = async (all_sites, concurrentPages) => {
-    for (let i = 0; i < all_sites.length; i += concurrentPages) {
-      const pagePromises = [];
-
-      for (let j = 0; j < concurrentPages; j++) {
-        const siteIndex = i + j;
-        if (siteIndex < all_sites.length) {
-          pagePromises.push(processEmail(all_sites[siteIndex]));
-        }
-      }
-
-      for (const pagePromise of pagePromises) {
-        await pagePromise;
-      }
-    }
-  };
-
-  const processPage = async (current_page) => {
-    const new_page = await browser.newPage();
     try {
-      await new_page.goto(
-        `https://career.habr.com/vacancies?page=${current_page}`,
-      );
-      bar1.update(40);
+      chr(".vacancy-card__title-link").each((index, element) => {
+        const name = chr(element).text().trim();
 
-      const array_name = await new_page.evaluate(() => {
-        const name = Array.from(
-          document.querySelectorAll(".vacancy-card__title-link"),
-          (el) => el.textContent,
-        );
-        return name;
+        const link = chr(element).attr("href");
+
+        const vacancyInfo = `${name} - https://career.habr.com${link}`;
+
+        console.log(`Получили данные - ${vacancyInfo}`);
+
+        links_combined.push(vacancyInfo);
+
+        all_links.push(`https://career.habr.com${link}`);
       });
-
-      const array_links = await new_page.evaluate(() => {
-        const links = Array.from(
-          document.querySelectorAll(".vacancy-card__title-link"),
-          (el) => el.getAttribute("href"),
-        );
-        return links;
-      });
-
-      const pageLinksCombined = array_name.map(
-        (name, index) =>
-          `${name} - https://career.habr.com${array_links[index]}`,
-      );
-
-      links_combined = links_combined.concat(pageLinksCombined);
-      all_links = all_links.concat(
-        array_links.map((link) => `https://career.habr.com${link}`),
-      );
     } catch (error) {
+      console.error(`Ошибка при загрузке страницы: ${error.message}`);
+
       fs.appendFileSync(
         "error.txt",
-        `${date} - Ошибка при открытии страницы https://career.habr.com/vacancies?page=${current_page}. ${error.message}\n`,
+        `${date} - Ошибка при загрузке страницы: ${error.message}\n`,
         "utf-8",
       );
-    } finally {
-      await new_page.close();
     }
-  };
+  }
 
-  /*
-  Используйте processPagesConcurrently для обработки 10 страниц одновременно
-  1 значение - начало
-  2 значение - конец
-  3 значение - шаг
-  */
+  console.log("\n\n2. Получаем список сайтов\n\n");
 
-  await processPagesConcurrently(1, 113, 15);
+  for (let i = 0; i < all_links.length; i++) {
+    try {
+      const response = await axios.get(all_links[i]);
+
+      const chr = cheerio.load(response.data);
+
+      const site_company = chr(".company_site").text().trim();
+
+      if (site_company && !all_sites.includes(`https://` + site_company)) {
+        all_sites.push("https://" + site_company);
+        console.log(`Получили сайт - https://${site_company}`);
+      }
+    } catch (error) {
+      console.log(`Не удалось получить сайт - ${all_links[i]}`);
+
+      fs.appendFileSync(
+        "error.txt",
+        `${date} - Ошибка при загрузке страницы: ${error.message}\n`,
+        "utf-8",
+      );
+    }
+  }
+
+  console.log("\n\n3. Получаем список почтовых ящиков\n\n");
+
+  for (let i = 0; i < all_sites.length; i++) {
+    try {
+      const response = await axios.get(`${all_sites[i]}`);
+
+      const chr = cheerio.load(response.data);
+
+      const emailElement = chr('[href*="mailto"]');
+
+      if (emailElement.length > 0) {
+        const hrefValue = emailElement.attr("href");
+        const email = hrefValue.replace("mailto:", "");
+
+        if (!all_mail.includes(email)) {
+          all_mail.push(email);
+          console.log(`Получили почту - ${email}`);
+        }
+      }
+    } catch (error) {
+      console.error(`Ошибка при загрузке страницы: ${all_sites[i]}`);
+
+      fs.appendFileSync(
+        "error.txt",
+        `${date} - Ошибка при загрузке страницы: ${error.message}\n`,
+        "utf-8",
+      );
+    }
+  }
 
   data.vacancies = links_combined.map((link) => link.replace(/ - /, " - "));
-  fs.writeFileSync(`data.json`, JSON.stringify(data, null, 2), "utf-8");
-
-  const processSite = async (link) => {
-    const new_page = await browser.newPage();
-    try {
-      await new_page.goto(link);
-
-      const array_site = await new_page.evaluate(() => {
-        const site_company = document.querySelector(".company_site");
-        return site_company ? site_company.textContent : null;
-      });
-
-      if (array_site && !all_sites.includes(array_site)) {
-        all_sites.push(array_site);
-      }
-    } catch (error) {
-      fs.appendFileSync(
-        "error.txt",
-        `${date} - Ошибка при открытии страницы ${link}. ${error.message}\n`,
-        "utf-8",
-      );
-    } finally {
-      await new_page.close();
-    }
-  };
-
-  await processSitesConcurrently(all_links, 25);
-
   data.sites = all_sites;
-  fs.writeFileSync(`data.json`, JSON.stringify(data, null, 2), "utf-8");
-
-  const processEmail = async (site) => {
-    const new_page = await browser.newPage();
-    try {
-      await new_page.goto(`https://${site}`);
-      fs.appendFileSync(
-        "log.txt",
-        `${date} - Страница успешно открыта ${site}\n`,
-        "utf-8",
-      );
-
-      const hrefValue = await new_page.evaluate(() => {
-        const elementsWithHref = document.querySelectorAll('[href*="mailto"]');
-        if (elementsWithHref.length > 0) {
-          return elementsWithHref[0].getAttribute("href");
-        }
-        return null;
-      });
-
-      if (hrefValue !== null) {
-        all_mail.push(hrefValue.replace(/mailto:/g, ""));
-      }
-    } catch (error) {
-      fs.appendFileSync(
-        "error.txt",
-        `${date} - Ошибка при открытии страницы ${site}. ${error.message}\n`,
-        "utf-8",
-      );
-    } finally {
-      await new_page.close();
-    }
-  };
-
-  await processEmailsConcurrently(all_sites, 25);
-
   data.mail = all_mail;
   fs.writeFileSync(`data.json`, JSON.stringify(data, null, 2), "utf-8");
 
-  bar1.update(100);
-  bar1.stop();
-
-  await browser.close();
   console.log("Создан файл data.json");
-  console.log("Парсер закончил работу успешно");
+  console.log("Парсер успешно завершен");
 };
 
 parser();
